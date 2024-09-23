f453682de13c0e5f4837be8be449f754758e1886
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-09-23 20:59:54 +0300

    version_to_kirill

--- calc/calc.go
+++ calc/calc.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func twoNumOp(num1 int, num2 int, op byte) int {
+func twoNumOp(num1 float64, num2 float64, op byte) float64 {
 	switch op {
 	case '+':
 		return num1 + num2
@@ -22,7 +22,7 @@ func twoNumOp(num1 int, num2 int, op byte) int {
 }
 
 func compareOp(op1 byte, op2 byte) bool {
-	fmt.Println(string(op1), string(op2))
+	// fmt.Println(string(op1), string(op2))
 	if op1 == op2 {
 		return true
 	} else if (op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'|| op2 == '*'|| op2 == '/') {
@@ -35,12 +35,17 @@ func compareOp(op1 byte, op2 byte) bool {
 	return false
 }
 
-func calculate (expression string) int {
+func calculate (expression string) float64 {
 	var operations []byte
-	var numbers []int
+	var numbers []float64
 	var one_number string
+	var minus_braket int
 	for i := 0; i < len(expression); i++ {
 		if expression[i] == '(' {
+			if len(numbers) == 0 && len(operations) >= 1 && operations[len(operations) - 1] == '-' {
+				minus_braket++
+				operations = operations[:len(operations) - 1]
+			}
 			operations = append(operations, expression[i])
 		} else if expression[i] == ')' {
 			for len(operations) > 0 && operations[len(operations) - 1] != '(' {
@@ -48,19 +53,33 @@ func calculate (expression string) int {
 				numbers = numbers[:len(numbers) - 1]
 				num2 := numbers[len(numbers) - 1]
 				numbers = numbers[:len(numbers) - 1]
-				oneOperaion := operations[len(operations) - 1]
+				oneOperation := operations[len(operations) - 1]
 				operations = operations[:len(operations) - 1]
-				numbers = append(numbers, twoNumOp(num2, num1, oneOperaion))
+				// fmt.Println(num2,num1,twoNumOp(num2, num1, oneOperation))
+				numbers = append(numbers, twoNumOp(num2, num1, oneOperation))
+			}
+			if minus_braket !=0 && len(numbers) == 1 {
+				numbers[len(numbers) - 1] = -numbers[len(numbers) - 1]
+				minus_braket--
 			}
 			operations = operations[:len(operations) - 1]
 		} else if expression[i] >= '0' && expression[i] <= '9' {
 			one_number = ""
+			if len(operations) > 0 && operations[len(operations) - 1] == '-' {
+				if len(numbers) == 0 {
+					operations = operations[:len(operations) - 1]
+					one_number = one_number + "-"
+				} else if len(operations) > 1 && expression[i-1] == '(' {
+					operations = operations[:len(operations) - 1]
+					one_number = one_number + "-"
+				}
+			}
 			for len(expression) > i && expression[i] >= '0' && expression[i] <= '9' {
 				one_number = one_number + string(expression[i])
 				i++
 			}
 			i--
-			num, _ := strconv.Atoi(one_number)
+			num, _ := strconv.ParseFloat(one_number,64)
 			// fmt.Println(num)
 			numbers = append(numbers, num)
 		} else {
@@ -81,16 +100,17 @@ func calculate (expression string) int {
 			operations = append(operations, expression[i])
 		}
 	}
-
+	fmt.Println(string(operations))
 	for len(operations) > 0 {
 		// fmt.Println(numbers)
 		num1 := numbers[len(numbers) - 1]
 		numbers = numbers[:len(numbers) - 1]
 		num2 := numbers[len(numbers) - 1]
 		numbers = numbers[:len(numbers) - 1]
-		oneOperaion := operations[len(operations) - 1]
+		oneOperation := operations[len(operations) - 1]
 		operations = operations[:len(operations) - 1]
-		numbers = append(numbers, twoNumOp(num2, num1, oneOperaion))
+		// fmt.Println(num2,num1,twoNumOp(num2, num1, oneOperation))
+		numbers = append(numbers, twoNumOp(num2, num1, oneOperation))
 	}
 	return numbers[0]
 }
@@ -99,5 +119,6 @@ func main() {
 	data := bufio.NewScanner(os.Stdin)
 	data.Scan()
 	exspression := data.Text()
+	// exspression := "-(-11-(1*20/2)-11/2*3)"
 	fmt.Println(calculate(exspression))
 }
---
+++ calc/calc_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCalculate_Plus(t *testing.T) {
+	expression := "41+3"
+	expected := float64(44)
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestCalculate_Minus(t *testing.T) {
+	expression := "41-37"
+	expected :=  float64(4)
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestCalculate_Multiplication(t *testing.T) {
+	expression := "41*37"
+	expected :=  float64(1517)
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestCalculate_Divide(t *testing.T) {
+	expression := "256/16"
+	expected :=  float64(16)
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestCalculate_Brackets(t *testing.T) {
+	expression := "35+4*(17-7)-41*30"
+	expected :=  float64(-1155)
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestCalculate_MinusBrackets(t *testing.T) {
+	expression := "-(-11-(1*20/2)-11/2*3)"
+	expected := 37.5
+
+	result := calculate(expression)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
